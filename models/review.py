ecd4f923f9defc4f8d4237242f4bd7b055b2bcc1
Mbicha <mbithicharlse@gmail.com>
2022-08-07 17:14:51 +0300

    Updated review class

--- models/review.py
+++ models/review.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" 
-    Import modules and packages 
+"""
+Import modules and packages
 """
 
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """
+    Define Review class attributes
+    """
     place_id = ""
     user_id = ""
     text = ""
